33b2d7aec3c5d36a71aee4f2ad557b1dac13b9b8
KenanBolat <kenan23@gmail.com>
2020-05-08 13:58:27 +0300

    Change mod

--- source.py
+++ source.py
@@ -10,31 +10,44 @@ from tables import *
 from pyhdf.SD import SD, SDC
 from pyresample import load_area, save_quicklook, SwathDefinition
 from pyresample.kd_tree import resample_nearest
+
 start = datetime.datetime.now()
 file_flag = 'NPP_VMAES_L1'
 
 process_path = '/external/b/viirs/set3'
-for file_ in glob.glob1(process_path,"{}*h*".format(file_flag)):
-    print(file_)
-    inner_file =os.path.join(process_path,file_)
-    f = SD(inner_file,SDC.READ)
+
+lats = []
+data = []
+for en, file_ in enumerate(glob.glob1(process_path, "{}*h*".format(file_flag))):
+
+    print(file_, en)
+    inner_file = os.path.join(process_path, file_)
+    f = SD(inner_file, SDC.READ)
     # f = h5py.File(os.path.join(process_path,file_),'r')
     # f = open_file(inner_file, mode="r", title="VIIRS")
     lat = []
     lon = []
-    Radiance_M1 = []
+    dat = []
     lat = f.select('Latitude').get()  # select sds
     lon = f.select('Longitude').get()  # select sds
-    Radiance_M1 = f.select('Radiance_M1').get()  # select sds
-    swath_def = pyresample.geometry.SwathDefinition(lons=lon, lats=lat, nprocs=4)
-    area_def = load_area('areas.yaml', 'worldeqc3km70')
-    result = resample_nearest(swath_def, Radiance_M1, area_def, radius_of_influence=50000,  nprocs=20)
-    print(f.keys())
-    # f.close()
-    break
+    dat = f.select('Radiance_M1').get()  # select sds
+    if en == 0:
+        lats = numpy.vstack([lat, lat])
+        lons = numpy.vstack([lon, lon])
+        data = numpy.vstack([dat, dat])
+    else:
+        lats = numpy.vstack([lats, lat])
+        lons = numpy.vstack([lons, lon])
+        data = numpy.vstack([data, dat])
+
+swath_def = pyresample.geometry.SwathDefinition(lons=lon, lats=lat, nprocs=4)
+area_def = load_area('areas.yaml', 'worldeqc3km70')
+result = resample_nearest(swath_def, data, area_def, radius_of_influence=50000, nprocs=30)
+print(f.keys())
+# f.close()
 
 
-
-
+flag = "EIIRP"
 end = datetime.datetime.now()
 print(end - start)
+print("{}".format(flag))
